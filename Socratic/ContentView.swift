efa984bd1df75545b5932c912b4fdd9d7ce15a79
Henry Anyimadu <henry@whatsfabric.com>
2025-03-17 15:30:15 -0500

    Initial Commit

---
+++ Socratic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Socratic
+//
+//  Created by Henry Anyimadu on 3/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Socratic/SocraticApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocraticApp.swift
+//  Socratic
+//
+//  Created by Henry Anyimadu on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SocraticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
